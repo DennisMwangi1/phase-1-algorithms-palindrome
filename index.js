fb5dc279ad728a0519039e69c0424baa37ee0282
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-16 00:04:21 +0300

    commit tree 0682c0db86745789abbae613bf073fd9ae472844

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+  for (let i = 0;i<word.length ; i++) {
+    if (word[i]==word[word.length-i-1]) {
+    return true
+    }else{
+      return false
+    }
+  }
 }
 
 /* 
-  Add your pseudocode here
+  use a for loop to iterate through the characters of the word
+  use an if statement to compare the first character and the last character of the word as the iteration happens
+  return true if they match
+  returns false if they dont
+
 */
 
 /*
-  Add written explanation of your solution here
+  When a word is passed as an arguement to the function isPalindrome,,a for loop is initiated on the word.An if statement compares the first and last characters of the word as it is being iterated through and returns true if the letters match and false if the letters dont match
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,7 +27,7 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
+  
   console.log("");
 
   console.log("Expecting: false");
